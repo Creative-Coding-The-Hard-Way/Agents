662528c4aea86821ccbdc47959bc2319f125703c
BradLyman <BradLyman@users.noreply.github.com>
2021-05-02 01:29:07 +0000

    deploy: f719dbabc659962fc618349d95a5712afabfcbd8

--- implementors/agents/simulation/trait.Simulation.js
+++ implementors/agents/simulation/trait.Simulation.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["a2_async_sim"] = [{"text":"impl <a class=\"trait\" href=\"agents/simulation/trait.Simulation.html\" title=\"trait agents::simulation::Simulation\">Simulation</a> for <a class=\"struct\" href=\"a2_async_sim/struct.VehicleWorld.html\" title=\"struct a2_async_sim::VehicleWorld\">VehicleWorld</a>","synthetic":false,"types":["a2_async_sim::VehicleWorld"]}];
+implementors["a2_async_sim"] = [{"text":"impl Simulation for <a class=\"struct\" href=\"a2_async_sim/struct.VehicleWorld.html\" title=\"struct a2_async_sim::VehicleWorld\">VehicleWorld</a>","synthetic":false,"types":["a2_async_sim::VehicleWorld"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
